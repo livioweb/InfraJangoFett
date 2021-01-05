a9b6a450b2e6a374b2ea355f6e67fa40210f4d3d
LIVIO RODRIGUES LOPES <livio.lopes@armateus.com.br>
2021-01-05 11:15:00 -0300

    add all ambiente monitoraments with grafana, influxDb and Netdata

--- machines_ansible/app/script.sh
+++ machines_ansible/app/script.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+FOLDER="app"
+#PROJETO="grafanax"
 
-cd /vagrant/app
+cd /vagrant/$FOLDER
 docker build -t my-hello-world .
 docker run -d -p 8080:8080 my-hello-world
+
+docker-compose up -d
+
+curl -G http://172.17.177.48:8086/query --data-urlencode "q=CREATE DATABASE app"
 
 #docker run hello-world
